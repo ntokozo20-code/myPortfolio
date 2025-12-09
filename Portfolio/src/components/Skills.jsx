65c372a37e1b83b5757449a73d82b8bffc6f26c2
ntokozo20-code <ntokozokhumza09@gmail.com>
2025-12-09 13:00:57 +0200

    edited SKILLS

--- Portfolio/src/components/Skills.jsx
+++ Portfolio/src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Skills() {
   return (
   
-    <div className="flex items-center justify-center h-screen bg-cream white">
+    <div className="flex items-center justify-center h-screen bg-white">
       <div className="flex flex-col items-center px-10">
         <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold tracking-tight mb-10 text-orange-400">Skills & Expertise</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
